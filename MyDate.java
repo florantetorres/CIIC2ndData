e9eb6c3574a99ac0d3b52ad11a03c299f373e6e6
RedTorres <155429228+florantetorres@users.noreply.github.com>
2025-07-10 23:26:34 +0800

    Add files via upload

---
+++ MyDate.java
@@ -0,0 +1,17 @@
+package Tasksheet1_2_1;
+import java.text.SimpleDateFormat;  
+import java.util.*;
+public class MyDate {
+    public static void main(String[] args) {
+        int day = 12;
+        int month = 11;
+        int year = 2012;
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+        Date date = calendar.getTime();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        String formattedDate = dateFormat.format(date);
+        System.out.println(formattedDate);
+    }
+}
